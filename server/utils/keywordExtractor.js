c7ac4a4ee2475e2aa91afff4e83302dd754303f2
Rajesh <124452143+Rajesh-cpy@users.noreply.github.com>
2026-08-17 12:40:56 +0530

    Update extractKeywords to return unique keywords
    Refactor extractKeywords function to return unique keywords.

--- server/utils/keywordExtractor.js
+++ server/utils/keywordExtractor.js
@@ -1,5 +1,9 @@
-export const extractKeywords = (text) => {
-  return text
-    .toLowerCase()
-    .match(/\b[a-z]{3,}\b/g) || [];
+export const extractKeywords = (text = "") => {
+  return [
+    ...new Set(
+      text
+        .toLowerCase()
+        .match(/\b[a-z]{3,}\b/g) || []
+    ),
+  ];
 };
